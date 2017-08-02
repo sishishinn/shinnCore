afd382c79f25d1d9421ef7abfa45d4b949ff0803
sishishinn <381081781@qq.com>
2017-08-02 10:53:29 +0800

    测试提交

--- src/main/java/com/sishishinn/HelloWorld.java
+++ src/main/java/com/sishishinn/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	@Autowired
 	private UserManager userManager;
 	
-	/**
+	/**测试提交
      * 1. 使用RequestMapping注解来映射请求的URL
      * 2. 返回值会通过视图解析器解析为实际的物理视图, 对于InternalResourceViewResolver视图解析器，会做如下解析
      * 通过prefix+returnVal+suffix 这样的方式得到实际的物理视图，然后会转发操作
